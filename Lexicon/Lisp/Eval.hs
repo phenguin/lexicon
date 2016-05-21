e753d82066c53e948bd87794d248d75af2555b21
Justin Cullen <jcullen@alum.mit.edu>
2016-05-20 23:26:02 -0400

    Very basic evaluator done

--- Lexicon/Lisp/Eval.hs
+++ Lexicon/Lisp/Eval.hs
@@ -7,14 +7,19 @@ import qualified Data.Map as M
 import Data.Typeable
 import Data.Fix
 import Data.Maybe
-
+import Data.Either
 
 type VarName = Text
+
+maybeToEither :: b -> Maybe a -> Either b a
+maybeToEither x my = case my of
+  Nothing -> Left x
+  Just y -> Right y
 
 newtype Frame = Frame {frameMap :: Map VarName Value}
 type Env = [Frame]
 
-data Value = Nil | Symbol Text | Number Integer | Boolean Bool | Proc ([Value] -> Value)
+data Value = Nil | Symbol Text | Number Integer | Boolean Bool | Proc ([Value] -> Either ErrorMsg Value)
 instance Show Value where
   show v = case v of
     Nil -> "nil"
@@ -40,28 +45,30 @@ lastMay xs = case xs of
   [x] -> Just x
   (_:xs) -> lastMay xs
 
-eval' :: Env -> ExprF Value -> Value
+type ErrorMsg = String
+
+eval' :: Env -> ExprF Value -> Either ErrorMsg Value
 eval' env expr = case expr of
-           N i -> Number i
-           B b -> Boolean b
-           V name -> fromMaybe (error "Variable not found") $ envFind env name
-           Lambda vars expr -> Proc $ \vals ->
+           N i -> return $ Number i
+           B b -> return $ Boolean b
+           V name -> maybeToEither "Variable not found" $ envFind env name
+           Lambda vars expr -> return . Proc $ \vals ->
              eval (makeEnv vars vals env) expr
-           Begin exprs -> fromMaybe Nil (lastMay exprs)
+           Begin exprs -> return $ fromMaybe Nil (lastMay exprs)
            Cond pairs -> case pairs of
-                           [] -> Nil
-                           ((Boolean b, v):rest) -> if b then v else eval' env (Cond rest)
-                           (_:rest) -> eval' env (Cond rest)
+                           [] -> return Nil
+                           ((Boolean b, v):rest) -> if b then return v else eval' env (Cond rest)
+                           (_:rest) -> Left $ "Non-boolean Cond condition"
            If pred tval fval -> case pred of
-             Boolean True -> tval
-             Boolean False -> fval
-             _ -> error "IF error"
+             Boolean True -> return tval
+             Boolean False -> return fval
+             _ -> Left "Non boolean valued if statement predicate"
            Ap func args -> case func of
              Proc f -> f args
-             _ -> error "Tried to apply non-function"
+             _ -> Left "Tried to apply non-function"
 
-eval :: Env -> Expr -> Value
-eval env = cata (eval' env)
+eval :: Env -> Expr -> Either ErrorMsg Value
+eval env = cataM (eval' env)
 
 makeEnv :: [VarName] -> [Value] -> Env -> Env
 makeEnv vars vals outer = Frame (M.fromList (zip vars vals)) : outer
@@ -89,7 +96,7 @@ sumvals vs = Number . sum . catMaybes . map pred $ vs
 
 
 testEnv :: Env
-testEnv = initEnv ["a", "b", "c", "+"] $ map Number [1, 10, 100] ++ map Proc [sumvals]
+testEnv = initEnv ["a", "b", "c", "+"] $ map Number [1, 10, 100] ++ map (Proc . (return .)) [sumvals]
 
 test :: IO ()
 test = print ("hi" :: Text)
